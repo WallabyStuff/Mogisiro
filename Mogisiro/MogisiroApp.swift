b98065f241523f9cbb0efa790268b73f212d1cda
Wallaby <avocado34.131@gmail.com>
2023-05-17 18:03:22 +0900

    Initial Commit

---
+++ Mogisiro/MogisiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogisiroApp.swift
+//  Mogisiro
+//
+//  Created by 이승기 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct MogisiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
